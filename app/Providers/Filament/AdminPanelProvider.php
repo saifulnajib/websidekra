5f749d74b3e8c9a85fb146d4acd90410769559ef
Saiful Najib <saifulnajib@Saifuls-MacBook-Air.local>
2025-12-04 15:34:04 +0700

    update profile module

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -36,7 +36,7 @@ class AdminPanelProvider extends PanelProvider
                         BreezyCore::make()
                         ->myProfile(
                             shouldRegisterUserMenu: true, // Sets the 'account' link in the panel User Menu (default = true)
-                            userMenuLabel: 'My Profile', // Customizes the 'account' link label in the panel User Menu (default = null)
+                            userMenuLabel: 'Profil Saya', // Customizes the 'account' link label in the panel User Menu (default = null)
                             shouldRegisterNavigation: false, // Adds a main navigation item for the My Profile page (default = false)
                             navigationGroup: 'Settings', // Sets the navigation group for the My Profile page (default = null)
                             hasAvatars: false, // Enables the avatar upload form component (default = false)
